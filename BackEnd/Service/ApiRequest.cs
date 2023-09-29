25bf1a5b5d5c23768b340da9b563e375c9e7d856
Kauã Freitas <kaaunt5@gmail.com>
2023-09-29 15:39:40 -0300

    Requisição da api e metodos para retorno do pokemon.

---
+++ BackEnd/Service/ApiRequest.cs
@@ -0,0 +1,85 @@
+﻿using GLib;
+using Newtonsoft.Json;
+using PokeApi.BackEnd.Entities;
+using PokeApiNet;
+using System.Drawing;
+using System.Net;
+
+namespace PokeApi.BackEnd.Service
+{
+    public class ApiRequest
+    {
+        private readonly PokeApiClient pokeApiClient = new PokeApiClient();
+
+        private int current = 0;
+
+        public Pokemon? GetPokemon(string pokename)
+        {
+            var client = new HttpClient();
+            Pokemon? pokemon;
+            string lowercase = pokename.ToLower();
+            string url = "https://pokeapi.co/api/v2/pokemon/" + lowercase;
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = client.SendAsync(request).Result;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var json = response.Content.ReadAsStringAsync().Result;
+                pokemon = JsonConvert.DeserializeObject<Pokemon>(json);
+                return pokemon;
+            }
+            else
+            {
+                Console.WriteLine("Pokemon não encontrado!.");
+                return null;
+            }
+        }
+
+        public List<Pokemon> GetPokeList()
+        {
+            var client = new HttpClient();
+            PokeList pokeList = new PokeList();
+            List<Pokemon> result = new List<Pokemon>();
+            string url = "https://pokeapi.co/api/v2/pokemon?limit=2&offset=" + current;
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = client.SendAsync(request).Result;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var json = response.Content.ReadAsStringAsync().Result;
+                pokeList = JsonConvert.DeserializeObject<PokeList>(json);
+                foreach (var pokemon in pokeList.result)
+                {
+                    var poke = GetPokemon(pokemon.Name);
+                    result.Add(poke);
+                }
+                current += 1;
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Pokemon não encontrado!.");
+                return null;
+            }
+        }
+
+        public byte[] GetPokemonSprite(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png";
+                var response = client.GetAsync(url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsByteArrayAsync().Result;
+                }
+                else
+                {
+                    Console.WriteLine("Pokémon não encontrado!");
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ BackEnd/Service/Methods.cs
@@ -0,0 +1,70 @@
+﻿using Gtk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApi.BackEnd.Service
+{
+    public class Methods
+    {
+        private readonly ApiRequest _apiRequest = new ApiRequest();
+
+        private void ModifyAllButtons(Fixed fix)
+        {
+            foreach (var button in fix.AllChildren)
+            {
+                if (button is Button)
+                {
+                    var btn = (Button)button;
+
+                    if (btn.Name == "BtnTest" || btn.Name == "BtnTest2")
+                    {
+                        var pokemonList = _apiRequest.GetPokeList();
+
+                        foreach (var pokemon in pokemonList)
+                        {
+                            Console.WriteLine(pokemon.Name);
+                            UpdateButtonImages(btn, pokemon.Id);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UpdateButtonImages(Button button, int id)
+        {
+            Image pokemonImage = GetImage(id);
+
+            if (pokemonImage != null)
+            {
+                button.Image = pokemonImage;
+            }
+        }
+
+        private Image GetImage(int id)
+        {
+            Image pokemonImage = new Image();
+            var imageBytes = _apiRequest.GetPokemonSprite(id);
+            if (imageBytes != null)
+            {
+                var pixbuf = new Gdk.Pixbuf(imageBytes);
+                if (pixbuf != null)
+                {
+                    pokemonImage.Pixbuf = pixbuf;
+                    return pokemonImage;
+                }
+                else
+                {
+                    Console.WriteLine("Erro ao criar o Pixbuf da imagem.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Pokémon não encontrado.");
+            }
+            return pokemonImage;
+        }
+    }
+}
