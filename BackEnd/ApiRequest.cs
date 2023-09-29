7f0a9337150d99d1ebba1052cf1d45ca8a9bfae6
Kauã Freitas <kaaunt5@gmail.com>
2023-09-29 09:46:15 -0300

    FirstCommit

---
+++ BackEnd/ApiRequest.cs
@@ -0,0 +1,21 @@
+﻿using PokeApiNet;
+
+namespace NowComesGtk.BackEnd
+{
+    internal class ApiRequest
+    {
+        private PokeApiClient pokeApiClient = new PokeApiClient();
+
+        public async Task<Pokemon> GetPokemon(string pokemonName)
+        {
+            Pokemon pokemon = await pokeApiClient.GetResourceAsync<Pokemon>(pokemonName);
+            return pokemon;
+        }
+
+        public async Task<PokemonSpecies> GetPokemonSpecies(string pokemonName)
+        {
+            PokemonSpecies pokemonSpecies = await pokeApiClient.GetResourceAsync<PokemonSpecies>(pokemonName);
+            return pokemonSpecies;
+        }
+    }
+}
